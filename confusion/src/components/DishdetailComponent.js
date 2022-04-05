01a7b645c4d63ab49d0916b79af17483df58802c
UralGuru <gizzatullinu@gmail.com>
2022-04-05 16:58:48 +0500

    Header and Footer

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import {Card,CardImg,CardText,CardTitle,CardBody} from "reactstrap"
+import React, {Component} from 'react';
+import {Card, CardImg, CardText, CardTitle, CardBody} from "reactstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 class Dishdetail extends Component {
@@ -16,70 +16,71 @@ class Dishdetail extends Component {
     }
 
 
-    renderDish=(dish)=>
-    {
-        if (dish!=null)
-        {
-            return(
-                <React.Fragment>
-                    <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg width="100%" src={dish.image} alt={dish.name} />
-                            <CardBody>
-                                <CardTitle>{dish.name}</CardTitle>
-                                <CardText>{dish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div className="col-12 col-md-5 m-1" >
-                        <h4>Comments</h4>
-                        {this.renderComments(dish.comments)}
-                    </div>
-                </React.Fragment>
+    renderDish = (dish) => {
+        if (dish != null) {
+            return (
+                <div className="container">
+                    <React.Fragment>
+                        <div className="col-12 col-md-5 m-1">
+                            <Card>
+                                <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                                <CardBody>
+                                    <CardTitle>{dish.name}</CardTitle>
+                                    <CardText>{dish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                            <div className="col-12 col-md-5 m-1">
+                                <h4>Comments</h4>
+                                {this.renderComments(dish.comments)}
+                            </div>
+
+                    </React.Fragment>
+                </div>
+
             )
-        }
-        else{
-            return(<div></div>)
+        } else {
+            return (<div></div>)
         }
     }
 
-    renderComments =(comments) =>
-    {
+    renderComments = (comments) => {
 
-        if (comments!=null)
-        {
-            const com = comments.map(co=>{
+        if (comments != null) {
+            const com = comments.map(co => {
 
-                    return(
+                    return (
+
                         <React.Fragment>
-                            <li>{co.comment}</li><br />
-                            <li>-- {co.author}, {this.formatDate(co.date)}</li><br />
+                            <li>{co.comment}</li>
+                            <br/>
+                            <li>-- {co.author}, {this.formatDate(co.date)}</li>
+                            <br/>
                         </React.Fragment>
+
+
                     )
 
                 }
             );
-            return(
-                <ul className="list-unstyled">
-                    {com}
-                </ul>
+            return (
+                    <ul className="list-unstyled">
+                        {com}
+                    </ul>
             )
-        }
-        else{
-            return(<div></div>)
+        } else {
+            return (<div></div>)
         }
     }
 
-    formatDate(date)
-    {
-        const option = {year: 'numeric', month: 'short', day: 'numeric' };
+    formatDate(date) {
+        const option = {year: 'numeric', month: 'short', day: 'numeric'};
         const date1 = new Date(date)
         const newdate = date1.toLocaleDateString("en-US", option)
         return newdate;
 
     }
 }
-
 
 
 export default Dishdetail;
