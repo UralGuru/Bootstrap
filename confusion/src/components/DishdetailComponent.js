e6b649646b1ffbea9e0ad48adef894302fb72586
UralGuru <gizzatullinu@gmail.com>
2022-04-10 17:03:42 +0500

    Redux Thunk

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
 import {Link} from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 class Dishdetail extends Component {
 
@@ -99,3 +100,25 @@ class Dishdetail extends Component {
 
 
 export default Dishdetail;
+
+
+/*if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null)    */
+
